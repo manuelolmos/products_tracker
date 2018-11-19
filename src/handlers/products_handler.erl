8f6a83da99ba590c1fa3fc25273bdc2b45df4b83
Manuel Olmos Gil <manuolmos88@gmail.com>
2018-11-18 23:42:54 -0300

    Create GET /products endpoint that will retrive all the products

--- src/handlers/products_handler.erl
+++ src/handlers/products_handler.erl
@@ -2,7 +2,12 @@
 -export([init/2]).
 
 init(Req0 = #{method := <<"GET">>}, State) ->
-    {ok, cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>},
-                          <<"hello world">>, Req0), State};
+    {ok, Products} = product:get_all(),
+    Json = jsx:encode(Products),
+    {ok, cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>},
+                          Json, Req0), State};
+init(Req0 = #{method := <<"POST">>}, State) ->
+    {ok, cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>}, <<"product created">>,
+                          Req0), State};
 init(Req0, State) ->
     {ok, cowboy_req:reply(400, Req0), State}.
